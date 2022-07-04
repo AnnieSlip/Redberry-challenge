bd86e340b34e4628ed2a3fd1b3df0080f6553d61
AnnieSlip <javakhishvili.an@gmail.com>
2022-07-04 14:11:37 +0400

    Styling of personal-info form

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
 "use strict";
-
-const startBtn = document.querySelector(".button");
-const page1 = document.querySelector(".page-1");
-const page2 = document.querySelector(".page-2");
-
-startBtn.addEventListener("click", function () {
-  page1.style.opacity = 0;
-  page2.classList.remove("hidden");
-});
+const form = document.querySelector("#personal-info-form");
