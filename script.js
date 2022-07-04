6d579882f2aa17ba2c46816ae73a93060a27795a
AnnieSlip <javakhishvili.an@gmail.com>
2022-07-04 14:59:08 +0400

    Styling input-fields

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 "use strict";
 const form = document.querySelector("#personal-info-form");
+const userName = document.querySelector("#name");
+const email = document.querySelector("#email");
+const date = document.querySelector("#date");
+const phone = document.querySelector("#phone");
+const input = document.querySelector(".input");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  validateForm();
+});
+
+function validateForm() {
+  //Username
+  if (userName.value.trim() == " ") {
+    input.classList.add("error");
+  }
+  //Email
+  //date
+  //phone number
+}
+
+function setError(element, errorMessage) {
+  const parent = element.parentElement;
+  parent.classList.add("error");
+}
