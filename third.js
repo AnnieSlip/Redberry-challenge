e149ada34189f1e1fe695261682f9ea20dfdf0fd
AnnieSlip <javakhishvili.an@gmail.com>
2022-07-06 23:53:58 +0400

    JSON RENDERING

--- third.js
+++ third.js
@@ -5,9 +5,13 @@ fetch("https://chess-tournament-api.devtest.ge/api/grandmasters")
   .then((completedata) => {
     console.log(completedata);
     let data1 = "";
+
     completedata.map((values) => {
-      data1 = ``;
+      console.log(values);
+      data1 = `
+      <option class="grandmaster" value="0">${values.name} <img src=${values.image} alt="img" class="grandmasters-img"></option>`;
     });
+    document.querySelector(".grandmaster").innerHTML = data1;
   })
   .catch((err) => {
     console.log(err);
